c9496b8c79c77b904ea29d364d1db62ae15978df
Zoe <zoewortzman@Zoes-MBP.lan>
2022-10-31 12:46:21 -0400

    adding name text box

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View, Image } from 'react-native';
 
-const HelloWorldApp = () => {
+const Welcome = () => {
   return (
-    <View
+    <View 
       style={{
-        flex: 1,
+        flex:1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        fontSize:10,
       }}>
-      <Text>Hello, world!</Text>
+      <Image 
+        source={{
+          uri:
+          'https://reactnative.dev/docs/assets/p_cat2.png'
+        }}
+        style={{
+          width:200,
+          height:200,
+
+        }}
+        />
+      <Text>Enter your name below</Text>
+      <TextInput 
+        style={{
+          height:50,
+          width:200,
+          borderColor:'navy',
+          borderWidth: 1
+        }}
+        />
     </View>
   )
 }
-export default HelloWorldApp;
+export default Welcome;
